4aba3c839256838c76de2c269647c43b6be644fd
Ryan Tsang <rvtsang@gmail.com>
2017-09-26 23:16:20 -0700

    added gui and left/right detection

---
+++ Assets/GUIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIController : MonoBehaviour {
+
+    public static string debugText = "test";
+    Rect debugTextLocation = new Rect(10, 10, 200, 100);
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(this.debugTextLocation, debugText);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            GUIController.debugText = "Right";
+        }
+
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            GUIController.debugText = "Left";
+        }
+    }
+}
+
